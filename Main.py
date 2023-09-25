287c04f125144bcfc1582ec86b490477a50b882c
suya172 <suya.in.internet@gmail.com>
2023-09-25 17:11:05 +0900

    abcd

--- Main.py
+++ Main.py
@@ -1,30 +1,36 @@
 import discord
-from discord.ext import tasks,commands
+from discord.ext import tasks
 from datetime import datetime
 from GenerateMessage import create_message
 import Config
+import asyncio
 
 TOKEN=Config.TOKEN
-CHANNEL_ID=Config.CHANNEL_ID
+CHANNEL_ID=int(Config.CHANNEL_ID)
 intents=discord.Intents.all()
 
 time_to_send='21:00'
 
-client=discord.Client(intents=intents)
+client = discord.Client(intents=intents)
 
 @client.event
 async def on_ready():
     print(f'起動しました。{datetime.now().isoformat()}')
 
+async def SendMessage():
+    channel = client.get_channel(CHANNEL_ID)
+    await channel.send(create_message())
+
 @tasks.loop(seconds=60)
-async def loop():
+async def checkTime():
     now=datetime.now().strftime('%H:%M')
     if now == time_to_send:
-        channel = client.get_channel(CHANNEL_ID)
-        message = create_message()
-        await channel.send(message)
+        await SendMessage()
 
+async def fn():
+    await checkTime.start()
 
-loop.start()
+loop_ = asyncio.get_event_loop()
+loop_.run_until_complete(fn())
 
 client.run(TOKEN)
