61828a47976f0edd974987fb41b14b1ea03144b0
suya172 <suya.in.internet@gmail.com>
2023-09-28 00:45:53 +0900

    aiueo

--- GenerateMessage.py
+++ GenerateMessage.py
@@ -1,39 +1,41 @@
 import requests
 import json
-from datetime import date,timedelta
+from datetime import date, timedelta
 import random
 
+
 def get_json(url):
-    today=date.today()
-    yesterday=today-timedelta(1)
+    today = date.today()
+    yesterday = today-timedelta(1)
 
     try:
-        res=requests.get(
+        res = requests.get(
             url,
             params={
-                "q":"vocaloid新曲リンク",
-                "targets":"tagsExact",
-                "fields":"contentId,title",
-                "filters[startTime][gte]":f"{yesterday.isoformat()}T00:00:00+09:00",
-                "filters[startTime][lt]":f"{today.isoformat()}T00:00:00+09:00",
-                "_sort":"startTime",
-                "_context":"vocaloid_everyday"
+                "q": "vocaloid新曲リンク",
+                "targets": "tagsExact",
+                "fields": "contentId,title",
+                "filters[startTime][gte]": f"{yesterday.isoformat()}T00:00:00+09:00",
+                "filters[startTime][lt]": f"{today.isoformat()}T00:00:00+09:00",
+                "_sort": "startTime",
+                "_context": "vocaloid_everyday"
             }
         )
         res.raise_for_status()
     except requests.exceptions.RequestException as e:
         print(f"\033[31mError:{e}\033[0m")
-        return json.dumps({'meta':{'status':000}})
+        return json.dumps({'meta': {'status': 000}})
     else:
         print("\033[33m取得完了\033[0m")
 
-    data=res.json()
+    data = res.json()
     return data
 
+
 def create_message(url='https://api.search.nicovideo.jp/api/v2/snapshot/video/contents/search'):
-    data=get_json(url=url)
-    status=data['meta']['status']
-    message =f'エラー： HTTPステータスコード{status}'
+    data = get_json(url=url)
+    status = data['meta']['status']
+    message = f'エラー： HTTPステータスコード{status}'
     if status == 000:
         message = 'エラー：通信に失敗しました。'
     elif status == 400:
@@ -45,7 +47,7 @@ def create_message(url='https://api.search.nicovideo.jp/api/v2/snapshot/video/co
     elif status == 200:
         contents_array = data['data']
         pick_content = random.choice(contents_array)
-        Id=pick_content['contentId']
-        title=pick_content['title']
-        message=f'今日の新着ボカロ曲だよ！\n\n{title}\nhttps://www.nicovideo.jp/watch/{Id}'
+        Id = pick_content['contentId']
+        title = pick_content['title']
+        message = f'今日の新着ボカロ曲だよ！\n\n{title}\nhttps://www.nicovideo.jp/watch/{Id}'
     return message
--- Main.py
+++ Main.py
@@ -1,36 +1,38 @@
+from datetime import datetime, timedelta
+
 import discord
+import pytz
 from discord.ext import tasks
-from datetime import datetime
-from GenerateMessage import create_message
+
 import Config
-import asyncio
+from GenerateMessage import create_message
 
-TOKEN=Config.TOKEN
-CHANNEL_ID=int(Config.CHANNEL_ID)
-intents=discord.Intents.all()
+TOKEN = Config.TOKEN
+CHANNEL_ID = int(Config.CHANNEL_ID)
+intents = discord.Intents.all()
 
-time_to_send='21:00'
+time_to_send = '21:00'
 
 client = discord.Client(intents=intents)
 
-@client.event
-async def on_ready():
-    print(f'起動しました。{datetime.now().isoformat()}')
 
 async def SendMessage():
     channel = client.get_channel(CHANNEL_ID)
-    await channel.send(create_message())
+    message = create_message()
+    await channel.send(message)
+    print(message)
+
 
 @tasks.loop(seconds=60)
 async def checkTime():
-    now=datetime.now().strftime('%H:%M')
+    now = datetime.now(pytz.timezone('Asia/Tokyo')).strftime('%H:%M')
     if now == time_to_send:
         await SendMessage()
 
-async def fn():
-    await checkTime.start()
 
-loop_ = asyncio.get_event_loop()
-loop_.run_until_complete(fn())
+@client.event
+async def on_ready():
+    print(f'起動しました。{(datetime.utcnow()+timedelta(hours=9)).isoformat()}')
+    await checkTime.start()
 
 client.run(TOKEN)
