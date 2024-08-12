8985c342d99a05e5902bad5258cf032c4d575627
suya172 <suya.in.internet@gmail.com>
2024-08-12 20:41:02 +0900

    fix: renderに移行する

--- Config.py
+++ Config.py
@@ -2,5 +2,5 @@ import dotenv
 dotenv.load_dotenv()
 
 import os
-TOKEN=os.environ.get('TOKEN')
-CHANNEL_ID=os.environ.get('CHANNEL_ID')
+TOKEN=os.environ['TOKEN']
+CHANNEL_ID=os.environ['CHANNEL_ID']
--- Main.py
+++ Main.py
@@ -6,6 +6,7 @@ from discord.ext import tasks
 
 import Config
 from GenerateMessage import create_message
+import asyncio
 
 TOKEN = Config.TOKEN
 CHANNEL_ID = int(Config.CHANNEL_ID)
@@ -23,16 +24,17 @@ async def SendMessage():
     print(message)
 
 
-@tasks.loop(seconds=60)
+@tasks.loop(seconds=30)
 async def checkTime():
     now = datetime.now(pytz.timezone('Asia/Tokyo')).strftime('%H:%M')
     if now == time_to_send:
         await SendMessage()
+        await asyncio.sleep(30)
 
 
 @client.event
 async def on_ready():
-    print(f'起動しました。{(datetime.utcnow()+timedelta(hours=9)).isoformat()}')
+    print(f'起動しました。{(datetime.now() + timedelta(hours=9)).isoformat()}')
     await checkTime.start()
 
 client.run(TOKEN)
